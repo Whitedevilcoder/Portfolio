82ebd93c680c5ba3fd9d5f5d5d189754556686ab
G Praveen Kumar <gpraveenkumar6405@gmail.com>
2026-02-09 23:26:45 +0530

    patch update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,34 @@
+import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
-import Contact from './components/Contact'; // <--- Import this
+import About from './components/About';          // <--- New
+import Contact from './components/Contact';
+import Footer from './components/Footer';        // <--- New
+import ScrollToTop from './components/ScrollToTop'; // <--- New
+import Background from './components/Background'; // <--- Import
+
 
 function App() {
   return (
-    <div className="bg-cyber-black min-h-screen text-white selection:bg-cyber-purple selection:text-white">
-      <Navbar />
-      <Hero />
-      <Projects />
-      <Contact /> {/* <--- Add this line */}
+    <div className="bg-cyber-black min-h-screen text-white relative">
+      
+      {/* The 3D Background (Fixed Position) */}
+      <Background /> 
+
+      {/* Your Main Content (Scrollable) */}
+      <div className="relative z-10">
+        <Navbar />
+        <Hero />
+        <Projects />
+        <div className="h-20"></div>
+        <About />
+        <div className="h-20"></div>
+        <Contact />
+        <Footer />
+      </div>
+
+      <ScrollToTop />
     </div>
   );
 }
